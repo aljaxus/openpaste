202a4c0ce6e794421dc2a9e3335a8fb98df459da
aljaxus <me@aljaxus.eu>
2019-03-07 23:11:54 +0100

    Create mod_savedata.js

---
+++ src/mod_savedata.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const getip = require('./mod_getip')
+
+const expressApp = express().use(express.static('./pastes'))
+
+const appServer = expressApp.listen(8101,'0.0.0.0' , () => {
+  const addr = 'http://'+getip.getLocalIP4()+':8100'
+  console.log('\n-> Server has started\n  |_ Console  @ '+addr+'/console.html\n  |_ Roulette @ '+addr+'/roulette.html\n\n\n')
+  eventEmitter.emit('runRoulette')
+})
+
+
+module.exports = {
+
+}
