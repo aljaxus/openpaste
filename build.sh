c2ce9a2667df781ed8b127a6a2eaba80e86d0ec0
aljaxus <me@aljaxus.eu>
2019-03-08 12:51:06 +0100

    updated

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ echo "-> removing openpaste container if exists"
 docker kill openpaste &>/dev/null
 docker rm openpaste &>/dev/null
 echo "-> starting new container called openpaste"
-docker run -ti --name openpaste --cpus 1 --detach --memory 128M aljaxus/openpaste
+docker run -ti --name openpaste --cpus 1 --detach aljaxus/openpaste
