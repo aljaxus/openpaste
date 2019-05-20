965abad73f02971b88498a383f91597f956d9d91
aljaxus <me@aljaxus.eu>
2019-05-20 10:49:16 +0200

    Added highlightjs, improvements

--- src/mod_servers.js
+++ src/mod_servers.js
@@ -6,6 +6,7 @@ const Enmap = require("enmap");
 const app = express()
   .use(express.static('./public'))
   .use(bodyParser.json({limit: '3mb'}))
+  
 const db = new Enmap({
   name: "pastes",
   fetchAll: true,
@@ -14,8 +15,8 @@ const db = new Enmap({
 
 db.defer.then( () => {
   console.log('-> enMap database is ready')
-  app.listen(8101, '0.0.0.0' , () => {
-    console.log('-> Paste server has started on '+'http://'+getip.getLocalIP4()+':8101')
+  app.listen(8080, '0.0.0.0' , () => {
+    console.log('-> Paste server has started on '+'http://'+getip.getLocalIP4()+':8080')
     if (process.env.ENV==='production') console.log('Press crtl+p & crtl+q to detach from container console')
   })
 })
