19ed3ff95366eb0ce1f4286743b0868cbbb452a5
aljaxus <me@aljaxus.eu>
2019-05-20 16:52:31 +0200

    update

--- src/mod_servers.js
+++ src/mod_servers.js
@@ -11,16 +11,15 @@ const db = new Enmap({
   name: "pastes",
   fetchAll: true,
   autoFetch: true
-});
+})
 
 db.defer.then( () => {
   console.log('-> enMap database is ready')
-  app.listen(8080, '0.0.0.0' , () => {
-    console.log('-> Paste server has started on '+'http://'+getip.getLocalIP4()+':8080')
+  app.listen(8101, '0.0.0.0', () => {
+    console.log('-> Paste server has started on http://'+getip.getLocalIP4()+':8101')
     if (process.env.ENV==='production') console.log('Press crtl+p & crtl+q to detach from container console')
   })
 })
-
 
 module.exports = {
   app: app,
